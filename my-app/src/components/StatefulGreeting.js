17c92b955cddbe23116964619d377530c894a13b
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-01-11 11:24:38 +0000

    FEAT: Use set state to update StatefulGreeting

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -5,16 +5,42 @@ class StatefulGreeting extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            introdution: "Hello",
+            introduction: "Hello",
             buttonText: "Exit",
+            count: 0,
         };
+    }
+
+    handleClick() {
+        this.setState((prevState, prevProps) =>{
+            return {
+                introduction: prevState.introduction === "Hello" ? "Goodbye" : "Hello",
+                buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit",
+            }
+        }, () =>{
+            console.log("Handle Click correctly called");
+            console.log("new state", this.state.introduction);
+        });
+    }
+
+    handleCounter() {
+        this.setState((prevState, prevProps) =>{
+            return {
+                count: prevState.count +1
+            }
+        }, () =>{
+            console.log("Handle counter correctly called");
+            console.log("new count is", this.state.count);
+        });
     }
 
     render() {
         return (
             <div>
-                <h1>{this.state.introdution} {this.props.name}, {this.props.greeting}</h1>
-                <button>{this.state.buttonText}</button>
+                <h1>{this.state.introduction} {this.props.name}, {this.props.greeting}</h1>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+                <button onClick={() => this.handleCounter()}>Counter</button>
+                <p>The counter has been clicked {this.state.count} times</p>
             </div>
 
         )
