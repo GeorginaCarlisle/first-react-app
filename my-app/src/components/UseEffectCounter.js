6719ab120492e80ea799b3618ebdc9814a0ce4c3
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-01-18 13:37:02 +0000

    FEAT: Conditional useEffect

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -4,7 +4,11 @@ function UseEffectCounter() {
     const [count, setCount] = useState(0);
     useEffect(() => {
         document.title = count;
-    });
+    }, [count]);
+    const [count2, setCount2] = useState(0);
+    useEffect(() => {
+        document.title = count2;
+    }, [count2]);
 
     return (
         <div>
@@ -13,6 +17,12 @@ function UseEffectCounter() {
             <button onClick={
                 () => setCount(count => count +1)
             }>Increase count</button>
+            
+            <h2>conditional effects used to prevent everything always updating regardless of what's needed</h2>
+            <p>The second count is: <span>{count2}</span></p>
+            <button onClick={
+                () => setCount2(count2 => count2 +1)
+            }>Increase count 2</button>
         </div>
     )
 }
